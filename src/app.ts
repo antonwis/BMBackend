c6c703bec1569d181100a6dab317e15f872739a1
antonwis <anton.wiskari@metropolia.fi>
2020-10-06 14:50:12 +0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,10 @@
 import express from "express";
-//import { Client } from "pg";
+import cors from 'cors';
 import apiRouter from "./routes/bands"
 
 const app = express();
 
-
-/*
-const client = new Client({
-  host: "localhost",
-  port: 5432,
-  user: "user",
-  password: "123456",
-  database: "metal",
-});
-
-client
-  .connect()
-  .then(() => console.log("connected"))
-  .catch((err) => console.error("connection error", err.stack));
-
-  
-client
-  .query("SELECT * FROM bands WHERE country ='Iceland'")
-  .then((result) => console.log(result.rows))
-  .catch((e) => console.error(e.stack))
-  .then(() => client.end());
-*/
-
+app.use(cors);
 app.get("/", (req, res, next) => {
   res.send("hello");
 });
